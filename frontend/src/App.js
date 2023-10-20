287df81b2701455ed606833a44d01059509cf152
DanSimonson <dansimonson@mariposaweb.net>
2023-10-20 06:50:37 -0500

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,45 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  const [transactions, setTransactions] = useState([]);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    let result = await axios.get("/api/categories");
-    setCategories(result.data);
-    result = await axios.get("/api/transactions");
-    setTransactions(result.data);
-  };
-  //console.log("products: ", products);
   return (
     <div className="App">
-      <h1>products</h1>
-      {/* {products.map((product) => (
-        <div>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
-      ))} */}
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+/* 
+{products.map((product) => (
+        <div>
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}   
+  fetchData()    
+  const fetchData = async () => {
+    let result = await axios.get("/api/categories");
+    setCategories(result.data);
+    result = await axios.get("/api/transactions");
+    setTransactions(result.data);
+  };    
+  
+  const [categories, setCategories] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    
+  }, []);
+      
+*/
