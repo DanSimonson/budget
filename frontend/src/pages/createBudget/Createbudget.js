54d5a7899f56bee8e803948e975c34be08b0b33a
DanSimonson <dansimonson@mariposaweb.net>
2023-11-04 16:10:44 -0500

    starting new apiSlice

--- frontend/src/pages/createBudget/Createbudget.js
+++ frontend/src/pages/createBudget/Createbudget.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faMinus,
-  faTrash,
-  faEdit,
-} from "@fortawesome/free-solid-svg-icons";
+
 import Createbudgetcss from "./Createbudget.module.css";
 
 function Createbudget() {
   return (
     <>
       <div>Createbudget</div>
-      <div>
-        <FontAwesomeIcon icon={faEdit} className={Createbudgetcss.edit} />
-        <FontAwesomeIcon icon={faPlus} className={Createbudgetcss.edit} />
-        <FontAwesomeIcon icon={faMinus} className={Createbudgetcss.edit} />
-        <FontAwesomeIcon icon={faTrash} className={Createbudgetcss.edit} />
-      </div>
     </>
   );
 }
