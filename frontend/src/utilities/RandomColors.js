6a91b5ea88bf02828ab5b820d585f9d14b29100d
DanSimonson <dansimonson@mariposaweb.net>
2023-11-08 09:59:27 -0600

    started working on generic random color array generation function

---
+++ frontend/src/utilities/RandomColors.js
@@ -0,0 +1,50 @@
+const RandomColors = (min, max) => {
+  //const [foundArray, setFoundArray] = useState([]);
+  let randomColorsArray = [
+    "#1e88e5",
+    "#3d5afe",
+    "#f44336",
+    "#01579b",
+    "#006064",
+    "#f44336",
+    "#263238",
+    "#2196f3",
+    "#795548",
+    "#e91e63",
+    "#ff5722",
+    "#3f51b5",
+    "#9c27b0",
+    "#e65100",
+    "#673ab7",
+    "#00838f",
+    "#9c27b0",
+    "#827717",
+    "#0277bd",
+    "#e91e63",
+    "#00695c",
+    "#311b92",
+    "#5c6bc0",
+    "#651fff",
+    "#1976d2",
+    "#0d47a1",
+    "#7986cb",
+    "#ff4081",
+    "#880e4f",
+    "#e040fb",
+  ];
+  let foundColorsArray = [""];
+  let num = 0;
+  while (num <= randomColorsArray.length - 1) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let randomIndex = Math.floor(step2) + min;
+    foundColorsArray.push(randomColorsArray[randomIndex]);
+    num++;
+  }
+  foundColorsArray.shift();
+  //console.log("foundColorsArray: ", foundColorsArray);
+  //setFoundArray(foundColorsArray);
+
+  return foundColorsArray;
+};
+export default RandomColors;
