957883c90429e533befea2d44d6373b52408dce2
DanSimonson <dansimonson@mariposaweb.net>
2023-11-14 15:11:24 -0600

    successfully serving frontend files to backend. Ready to push to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const categoryRoutes = require("./routes/categories");
 const transactionRoutes = require("./routes/transactions");
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // express app
 const app = express();
 
@@ -13,16 +25,14 @@ app.use(express.json());
 app.use("/api/categories", categoryRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-// connect to db
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("connected to database");
-    // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log("listening for requests on port", process.env.PORT);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(_dirname, "/frontend/build/index.html"))
+);
+
+// listen to port
+const port = process.env.PORT || 8338;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
