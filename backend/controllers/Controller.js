2f08b952bdb47505aa29b10024154022180ee298
DanSimonson <dansimonson@mariposaweb.net>
2023-11-21 10:24:04 -0600

    fixed typo

--- backend/controllers/Controller.js
+++ backend/controllers/Controller.js
@@ -134,7 +134,7 @@ const createTransaction = async (req, res) => {
 // update a workout
 const updateTransaction = async (req, res) => {
   const { id } = req.params;
-  
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "No such transaction" });
   }
