87179b7539520a399d243b9ddc79738ee8d0051f
DanSimonson <dansimonson@mariposaweb.net>
2023-11-23 13:39:49 -0600

    created dynamic updating with percentage bar showing available amount remaining

--- frontend/src/utilities/UpdateBar.js
+++ frontend/src/utilities/UpdateBar.js
@@ -1,3 +1,4 @@
+/* update bar to calculate and return percentage of budget available */
 export const UpdateBar = (calculation) => {
   let percentage = calculation[0].remainingAmount / calculation[0].amount;
   percentage = percentage * 100;
